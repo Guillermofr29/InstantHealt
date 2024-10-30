cedc3c11a445afed7a0f09fda541f5b7982843cc
GabrielCaamal26 <carlos.chancaamal@gmail.com>
2024-10-30 14:46:52 -0500

    Loading screen fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import "react-native-reanimated";
 import { Text, View } from "react-native";
 import Login from "@/app/login";
 import Home from "./(tabs)";
+import LoadingScreen from "./LoadingScreen";
 import { router, Slot, Stack } from "expo-router";
 import 'react-native-reanimated'
 import 'react-native-gesture-handler'

15fcd24c68452421d49a446e3743da6f51828c8d
GabrielCaamal26 <carlos.chancaamal@gmail.com>
2024-11-04 19:27:53 -0500

    Update Icon.tsx

--- Components/Icon.tsx
+++ Components/Icon.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
 import { Feather } from "@expo/vector-icons";
 
 export const Icon: { [key: string]: (props: any) => JSX.Element } = {
   index: (props: any) => <Feather name="home" size={30} {...props} />,
   course: (props: any) => <Feather name="book" size={30} {...props} />,
   call: (props: any) => <Feather name="phone" size={30} {...props} />,
-  chatbot: (props: any) => <Feather name="book" size={30} {...props} />,
+  chatbotScreen: (props: any) => <MaterialCommunityIcons name="robot-outline" size={30} color="black" {...props} />,
 };
